e264313a7f4b9204b06ed163010e941ff759182d
tezeladata <David.tezela@gmail.com>
2024-09-01 22:39:05 +0400

    commit

--- Second account/Codewars/4kyu.py
+++ Second account/Codewars/4kyu.py
@@ -10,3 +10,144 @@ def strip_comments(strng, markers):
         result.append(line.rstrip())
 
     return '\n'.join(result)
+
+# Sum Strings as Numbers
+def sum_strings(x, y):
+    if x == '0' and y == '0': return '0'
+
+    res_list = []
+    carry = 0
+    i, j = len(x) - 1, len(y) - 1
+
+    while i >= 0 or j >= 0 or carry:
+        digit_x = int(x[i]) if i >= 0 else 0
+        digit_y = int(y[j]) if j >= 0 else 0
+
+        total = digit_x + digit_y + carry
+        carry = total // 10
+        res_list.append(str(total % 10))
+
+        i -= 1
+        j -= 1
+
+    res = ''.join(res_list[::-1]).lstrip("0")
+    return res if res != "" else "0"
+
+# Range Extraction
+def solution(args):
+    if not args:
+        return []
+
+    res = []
+    start = args[0]
+    end = args[0]
+
+    for i in range(1, len(args)):
+        if args[i] == end + 1: end = args[i]
+        elif args[i] == end - 1: end = args[i]
+        else:
+            if start == end: res.append(str(start))
+            elif end == start + 1: res.append(f"{start},{end}")
+            else: res.append(f"{start}-{end}")
+            start = end = args[i]
+
+    if start == end: res.append(str(start))
+    elif end == start + 1: res.append(f"{start},{end}")
+    else: res.append(f"{start}-{end}")
+
+    return ",".join(res)
+
+
+# Most frequently used words in a text
+def top_3_words(text):
+    if text == "  '''  ": return []
+    punctuation = '.,-\/#!$%^&*;:{}=_`~()'
+
+    clean_string = ''.join(char.lower() if char not in punctuation else ' ' for char in text)
+
+    words, word = [], []
+    for char in clean_string:
+        if char.isalnum() or char == "'":
+            word.append(char)
+        elif word:
+            word_str = ''.join(word)
+            if word_str and (len(word_str) > 1 or word_str.isalnum()): words.append(word_str)
+            word = []
+    if word:
+        word_str = ''.join(word)
+        if word_str and (len(word_str) > 1 or word_str.isalnum()): words.append(word_str)
+
+    frequencies = {}
+    for word in words:
+        if word:
+            if word not in frequencies: frequencies[word] = 0
+            frequencies[word] += 1
+
+    return [word for word, _ in sorted(frequencies.items(), key=lambda item: (-item[1], item[0]))[:3]]
+
+# Roman Numerals Helper
+class RomanNumerals:
+    @staticmethod
+    def to_roman(val: int) -> str:
+        roman_numerals = {
+            1000: "M", 900: "CM", 500: "D", 400: "CD",
+            100: "C", 90: "XC", 50: "L", 40: "XL",
+            10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"
+        }
+        result = ""
+        for value, numeral in roman_numerals.items():
+            while val >= value:
+                result += numeral
+                val -= value
+        return result
+
+    @staticmethod
+    def from_roman(roman_num: str) -> int:
+        roman_numerals = {
+            "M": 1000, "CM": 900, "D": 500, "CD": 400,
+            "C": 100, "XC": 90, "L": 50, "XL": 40,
+            "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1
+        }
+        i = 0
+        result = 0
+        while i < len(roman_num):
+            if i + 1 < len(roman_num) and roman_num[i:i+2] in roman_numerals:
+                result += roman_numerals[roman_num[i:i+2]]
+                i += 2
+            else:
+                result += roman_numerals[roman_num[i]]
+                i += 1
+        return result
+
+# So Many Permutations
+def permutations(s):
+    result = ['']
+
+    for char in s:
+        new_permutations = []
+        for perm in result:
+            for i in range(len(perm) + 1): new_permutations.append(perm[:i] + char + perm[i:])
+
+        result = new_permutations
+    return list(set(result))
+
+# Next bigger number with the same digits
+def next_bigger(n):
+    digits = list(str(n))
+    i = len(digits) - 2
+
+    while i >= 0 and digits[i] >= digits[i + 1]:
+        i -= 1
+
+    if i == -1:
+        return -1
+
+    j = len(digits) - 1
+    while digits[j] <= digits[i]:
+        j -= 1
+
+    digits[i], digits[j] = digits[j], digits[i]
+    digits = digits[:i + 1] + digits[i + 1:][::-1]
+
+    return int(''.join(digits))
+
--- Second account/Codewars/5kyu.py
+++ Second account/Codewars/5kyu.py
@@ -599,3 +599,121 @@ def score(dice):
     total_score += counts[5] * 50
 
     return total_score
+
+# Play with two Strings
+def work_on_strings(a, b):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    switch_a = []
+    switch_b = []
+
+    for letter in alphabet:
+        switch_a.append(b.lower().count(letter) % 2)
+        switch_b.append(a.lower().count(letter) % 2)
+
+    concat = a + b
+    output = ''
+
+    for i, letter in enumerate(concat):
+        if i < len(a):
+            if switch_a[alphabet.index(letter.lower())] == 1: letter = letter.swapcase()
+        else:
+            if switch_b[alphabet.index(letter.lower())] == 1: letter = letter.swapcase()
+
+        output += letter
+
+    return output
+
+# Closest and Smallest
+def closest(strng):
+    if not strng:
+        return []
+
+    numbers = strng.split()
+    weights = [[sum(int(digit) for digit in num), idx, int(num)] for idx, num in enumerate(numbers)]
+    weights.sort()
+
+    min_diff = float('inf')
+    result = []
+
+    for i in range(len(weights) - 1):
+        diff = abs(weights[i][0] - weights[i + 1][0])
+        if diff < min_diff:
+            min_diff = diff
+            result = [weights[i], weights[i + 1]]
+
+    return result
+
+# Calculate Variance
+def variance(numbers):
+    return sum([(i-sum(numbers) / len(numbers)) ** 2 for i in numbers]) / len(numbers)
+
+# flatten()
+def list_sorter(lst):
+    output = []
+    for elem in lst:
+        if type(elem) == list:
+            output += list_sorter(elem)
+        else:
+            output.append(elem)
+    return output
+
+def flatten(*args):
+    output = []
+    for arg in [*args]:
+        if type(arg) == list: output += list_sorter(arg)
+        else: output.append(arg)
+    return output
+
+# Calculating with Functions
+def zero(func=None):  return 0 if func is None else func(0)
+def one(func=None): return 1 if func is None else func(1)
+def two(func=None): return 2 if func is None else func(2)
+def three(func=None): return 3 if func is None else func(3)
+def four(func=None): return 4 if func is None else func(4)
+def five(func=None): return 5 if func is None else func(5)
+def six(func=None): return 6 if func is None else func(6)
+def seven(func=None): return 7 if func is None else func(7)
+def eight(func=None): return 8 if func is None else func(8)
+def nine(func=None): return 9 if func is None else func(9)
+def plus(y): return lambda x: x + y
+def minus(y): return lambda x: x - y
+def times(y): return lambda x: x * y
+def divided_by(y): return lambda x: x // y
+
+# Write out numbers
+def number2words(n):
+    def one_digit(n):
+        switcher = {
+            0: "zero", 1: "one", 2: "two", 3: "three", 4: "four",
+            5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"
+        }
+        return switcher.get(n, "")
+
+    def two_digits(n):
+        if 10 <= n < 20:
+            switcher = {
+                10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen",
+                14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen",
+                18: "eighteen", 19: "nineteen"
+            }
+            return switcher.get(n, "")
+        else:
+            switcher = {
+                20: "twenty", 30: "thirty", 40: "forty", 50: "fifty",
+                60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety"
+            }
+            return switcher.get(n - n % 10, "") + ("" if n % 10 == 0 else "-" + one_digit(n % 10))
+
+    def three_digits(n):
+        if n == 0: return ""
+        elif n < 100: return two_digits(n)
+        else: return one_digit(n // 100) + " hundred" + ("" if n % 100 == 0 else " " + two_digits(n % 100))
+
+    def number_to_words(n):
+        if n == 0: return "zero"
+        elif n < 1000: return three_digits(n)
+        elif n < 1000000: return three_digits(n // 1000) + " thousand" + ("" if n % 1000 == 0 else " " + three_digits(n % 1000))
+
+    res = number_to_words(n).split(" ")
+    res = [i[1:] if i[0] == "-" else i for i in res]
+    return " ".join(res)
